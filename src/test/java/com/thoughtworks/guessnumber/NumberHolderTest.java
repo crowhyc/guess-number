c1bdac6396038c7c03cb821c7b1ec11072921e0c
Huangyicai <yicai.huang@thoughtworks.com>
2020-01-22 18:47:09 +0800

    [Add] achieve NumberHolder guess number 2 equals and 2 equals but wrong location.

--- src/test/java/com/thoughtworks/guessnumber/NumberHolderTest.java
+++ src/test/java/com/thoughtworks/guessnumber/NumberHolderTest.java
@@ -10,20 +10,27 @@ public class NumberHolderTest {
     public void return_0A0B_when_input_1234() {
         NumberHolder numberHolder = new NumberHolder();
         String result = numberHolder.guess("1234");
-        assertEquals(result, "0A0B");
+        assertEquals("0A0B", result);
     }
 
     @Test
     public void return_4A0B_when_input_5678() {
         NumberHolder numberHolder = new NumberHolder();
         String result = numberHolder.guess("5678");
-        assertEquals(result, "4A0B");
+        assertEquals("4A0B", result);
     }
 
     @Test
     public void return_0A4B_when_input_6785() {
         NumberHolder numberHolder = new NumberHolder();
         String result = numberHolder.guess("6785");
-        assertEquals(result, "0A4B");
+        assertEquals("0A4B", result);
+    }
+
+    @Test
+    public void return_2A2B_when_input_5876() {
+        NumberHolder numberHolder = new NumberHolder();
+        String result = numberHolder.guess("5876");
+        assertEquals("2A2B", result);
     }
 }
